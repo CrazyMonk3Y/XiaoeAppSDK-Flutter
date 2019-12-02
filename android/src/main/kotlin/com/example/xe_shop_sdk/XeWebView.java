0812888511574ea33388b3d303d06104b0f24e09
rickyyao <rickkyao@xiaoe-tech.com>
2019-12-02 20:05:38 +0800

    feat:适配AndroidX

--- android/src/main/kotlin/com/example/xe_shop_sdk/XeWebView.java
+++ android/src/main/kotlin/com/example/xe_shop_sdk/XeWebView.java
@@ -2,7 +2,7 @@ package com.example.xe_shop_sdk;
 
 import android.app.Activity;
 import android.content.Context;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.view.View;
 import com.xiaoe.shop.webcore.XEToken;
 import com.xiaoe.shop.webcore.bridge.JsBridgeListener;
